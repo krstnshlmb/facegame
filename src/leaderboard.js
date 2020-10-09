16e5308b82aefea5fb8c690261dbd19a5cf26b4f
Robert Kim <>
2020-10-10 03:10:06 +0900

    added sorting for leaderboard

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -5,17 +5,32 @@ document.addEventListener('lobby_started', e => {
 
 function startListeningForLeaderboard(){
     const leaderboard = document.getElementById('leaderboard');
-    leaderboard.innerHTML = '';
+    
     const leaderboardPlayer = document.getElementById('leaderboardPlayer');
 
     db.ref(`/games/${currentGameId}/players`).on('value', function(snapshot) {
         
+        leaderboard.innerHTML = "";
+
         const val = snapshot.val();
 
+        let players = [];
+
         for(const k in val) {
+            
+            const player = val[k];
+
+            players.push({
+                name: player.name,
+                score: player.score
+            })
+        }
+
+        players = players.sort((a,b)=> { return a.score > b.score });
+
+        for(const player of players) {
 
             let rowNode = document.createElement("tr");
-            const player = val[k];
             
             rowNode.innerHTML = 
             `
@@ -24,8 +39,8 @@ function startListeningForLeaderboard(){
             `;
 
             leaderboard.appendChild(rowNode);
-        }
 
+        }
     })
 
 }
