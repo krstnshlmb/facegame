b18d9ef9839637946e21852485b196f0a9e68add
Adil Botabekov <botabekov.adil@gmail.com>
2020-10-09 11:07:15 +0000

    some comments

--- src/database.js
+++ src/database.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         function(error){
             if(error){
                 alert('Something went wrong...');
+                return;
             } else {
                 console.log(gameID);
 
@@ -34,6 +35,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     function joinGame(username, gameID) {
 
+        // 1. Verify if game exists
+        // 2. Verify if nickname is taken
+
+        var gameChecker = db.ref('games/' + gameID);
+
+        gameChecker.transaction(function(currentData){
+            if(currentData === null){
+                console.log('OH SHIT!');
+            }
+        });
+
+        if(gameChecker){
+            console.log(gameChecker);
+        } else {
+            console.log('Oh, no!');
+        }
+
+
         const playerID = Date.now();
 
         db.ref('games/' + gameID + '/players/' + playerID).set({
@@ -44,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         function(error){
             if(error){
                 alert('Something went wrong...');
+                return;
             } else {
                 console.log(playerID + '(' + username + ')' + ' has connected succesfully');
                 return playerID;
@@ -52,17 +72,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     }
 
+    function deleteGame(){
+
+    }
+
+
+    
 
     var new_game_btn = document.getElementById('new_game');
-
     var join_game_btn = document.getElementById('join_game');
+
+
 
     new_game_btn.onclick = function(){
         createNewGame('Adil');
     }
 
     join_game_btn.onclick = function(){
-        joinGame('Robert', 0);
+        joinGame('Robert', 12000);
     } 
 
 });
